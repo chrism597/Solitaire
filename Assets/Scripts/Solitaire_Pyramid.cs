d8479c2c6a9412476c5716a217d732ff4faa7a64
chrism597 <111245238+chrism597@users.noreply.github.com>
2024-04-12 20:51:55 -0500

    Each of the cards in the deck now have an associated sprite to be displayed

--- Assets/Scripts/Solitaire_Pyramid.cs
+++ Assets/Scripts/Solitaire_Pyramid.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Solitaire_Pyramid : MonoBehaviour
 {
+    public Sprite[] cardFaces;
     public static string[] suits = new string[] { "C", "D", "H", "S" };
     public static string[] values = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
     public List<string> deck;
